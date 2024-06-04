3477c6bbd8fce6a8e1196455b1bf33c17865c30b
Di4r <119154247+DiarDev@users.noreply.github.com>
2024-06-04 14:06:53 +0700

    📌 Correct all the mistakes that I made nearly 1 year ago

--- src/BaorzK/JoinParticle/Main.php
+++ src/BaorzK/JoinParticle/Main.php
@@ -16,50 +16,52 @@ use pocketmine\world\particle\WaterParticle;
 use pocketmine\world\particle\LavaParticle;
 use pocketmine\world\particle\EndermanTeleportParticle;
 
+
 class Main extends PluginBase implements Listener{
+  
   public function onEnable(): void{
-    $this->saveDeafaultConfig;
-    $this->getServer()->getPluginManager()->registerEvent($this, $this);
+    $this->saveDefaultConfig();
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
     $this->getLogger()->info("JoinParticle is Enable");
   }
   
-  public function onDisable(){
-    $this->config->save();
+  public function onDisable(): void{
+    $this->getConfig()->save();
     $this->getLogger()->info("JoinParticle is Disable");
   }
   
   public function onJoin(PlayerJoinEvent $ev){
   $player = $ev->getPlayer();
   $world = $player->getWorldManager()->getWorld();
-  $x = $player->getLocation()->getX;
-  $y = $player->getLocation()->getY;
-  $z = $player->getLocation()->getZ;
+  $x = $player->getLocation()->getX();
+  $y = $player->getLocation()->getY();
+  $z = $player->getLocation()->getZ();
   //See if it is true or not!
-  if($this->getConfig->get("FlameParticle") === true){
+  if($this->getConfig()->get("FlameParticle") === true){
   $position = $player->getPosition()->add(0, 0, 0);
   $particle = $world->addParticle($position, new FlameParticle());
   }
-  if($this->getConfig->get("LavaParticle") === true){
+  if($this->getConfig()->get("LavaParticle") === true){
   $position = $player->getPosition()->add(0, 0, 0);
   $particle = $world->addParticle($position, new LavaParticle());
   }
-  if($this->getConfig->get("AngryVillageParticle") === true){
+  if($this->getConfig()->get("AngryVillageParticle") === true){
   $position = $player->getPosition()->add(0, 0, 0);
   $particle = $world->addParticle($position, new AngryVillageParticle());
   }
-  if($this->getConfig->get("InkParticle") === true){
+  if($this->getConfig()->get("InkParticle") === true){
   $position = $player->getPosition()->add(0, 0, 0);
   $particle = $world->addParticle($position, new InkParticle());
   }
-  if($this->getConfig->get("SmokeParticle") === true){
+  if($this->getConfig()->get("SmokeParticle") === true){
   $position = $player->getPosition()->add(0, 0, 0);
   $particle = $world->addParticle($position, new SmokeParticle());
   }
-  if($this->getConfig->get("WaterParticle") === true){
+  if($this->getConfig()->get("WaterParticle") === true){
   $position = $player->getPosition()->add(0, 0, 0);
   $particle = $world->addParticle($position, new WaterParticle());
   }
-  if($this->getConfig->get("EndermanTeleportParticle") === true){
+  if($this->getConfig()->get("EndermanTeleportParticle") === true){
   $position = $player->getPosition()->add(0, 0, 0);
   $particle = $world->addParticle($position, new EndermanTeleportParticle());
   }
