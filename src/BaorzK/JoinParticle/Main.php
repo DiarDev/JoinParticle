8aa6f96db926f125762ed18b8024af5423982559
Di4r <119154247+DiarDev@users.noreply.github.com>
2024-06-04 14:10:11 +0700

    📌 Correct all the mistakes that I made nearly 1 year ago

--- src/BaorzK/JoinParticle/Main.php
+++ src/BaorzK/JoinParticle/Main.php
@@ -5,12 +5,12 @@ namespace BaorzK\JoinParticle;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\world\World;
-use pocketmine\event\PlayerJoinEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener; 
 use pocketmine\world\particle\FlameParticle;
 use pocketmine\world\particle\HeartParticle;
 use pocketmine\world\particle\InkParticle;
-use pocketmine\world\particle\AngryVillageParticle;
+use pocketmine\world\particle\AngryVillagerParticle;
 use pocketmine\world\particle\SmokeParticle;
 use pocketmine\world\particle\WaterParticle;
 use pocketmine\world\particle\LavaParticle;
@@ -45,9 +45,9 @@ class Main extends PluginBase implements Listener{
   $position = $player->getPosition()->add(0, 0, 0);
   $particle = $world->addParticle($position, new LavaParticle());
   }
-  if($this->getConfig()->get("AngryVillageParticle") === true){
+  if($this->getConfig()->get("AngryVillagerParticle") === true){
   $position = $player->getPosition()->add(0, 0, 0);
-  $particle = $world->addParticle($position, new AngryVillageParticle());
+  $particle = $world->addParticle($position, new AngryVillagerParticle());
   }
   if($this->getConfig()->get("InkParticle") === true){
   $position = $player->getPosition()->add(0, 0, 0);
